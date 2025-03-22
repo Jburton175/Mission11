47224e6f923338cd3c4c8a8f507b21ac92a9d840
JBurton175 <jburton175@gmail.com>
2025-03-21 23:03:27 -0600

    added categories to filters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Booklist from "./Booklist";
+import CategoryFilter from "./CategoryFilter";
 
 function App() {
   return (
     <>
+      <CategoryFilter />
       <Booklist />
     </>
   );
