82bbba070601f713a1070bb157bd5cc861e40ffa
JBurton175 <jburton175@gmail.com>
2025-03-31 11:03:30 -0600

    fixed cart summary preventing deployment

--- frontend/src/components/CartSummary.tsx
+++ frontend/src/components/CartSummary.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { useCart } from "../context/CartContext";
-import { CartItem } from "../types/CartItem";
 
 function CartSummary() {
   const { cart } = useCart();
