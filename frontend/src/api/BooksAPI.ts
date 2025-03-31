d3709a369028d82ee19c1ca3bd32d678e31378c6
JBurton175 <jburton175@gmail.com>
2025-03-31 11:00:24 -0600

    Deploying fontend

--- frontend/src/api/BooksAPI.ts
+++ frontend/src/api/BooksAPI.ts
@@ -1,6 +1,6 @@
 import { Book } from "../types/Books";
 
-const API_URL = `https://localhost:5000/Books`;
+const API_URL = `https://mission13.azurewebsites.net/books`;
 
 interface FetchBooksResponse {
   books: Book[];
