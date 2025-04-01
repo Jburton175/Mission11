3517f559dd5d361179583d4dbec7b9daee5088a9
JBurton175 <jburton175@gmail.com>
2025-03-31 19:23:58 -0600

    changed api url for debugging

--- frontend/src/api/BooksAPI.ts
+++ frontend/src/api/BooksAPI.ts
@@ -1,6 +1,6 @@
 import { Book } from "../types/Books";
 
-const API_URL = `https://mission13.azurewebsites.net/books`;
+const API_URL = `https://mission13.azurewebsites.net/`;
 
 interface FetchBooksResponse {
   books: Book[];
